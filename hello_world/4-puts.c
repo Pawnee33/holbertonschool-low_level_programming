120be4d2c1f5963b07acddc703c0db67564321d6
Pawnee <pawneedefize@hotmail.com>
2025-10-20 14:42:28 +0000

    un programme C qui affiche exactement « Programmer, c'est comme construire un puzzle multilingue, suivi d'une nouvelle ligne

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
  * main -  entry point 
- *@puts: Display my texts
+ *
  * Return Always 0 (Success)
  */
 int main(void)
 {
-/*puts display texts*/
+/*puts - print "Programming is like building a multilingual puzzle"*/
 	puts("\"Programming is like building a multilingual puzzle");
-/*Return 0*/
+/*Return: 0*/
 	return (0);
 }
