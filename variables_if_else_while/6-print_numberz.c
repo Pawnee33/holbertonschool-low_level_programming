effa5754c7ca1f4840d4d15eb50b2e238d2ba9a3
Pawnee <pawneedefize@hotmail.com>
2025-10-21 13:57:11 +0000

    Affiche tous les nombres avec un putchar

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - My principal function
+ * int - My variable 'i' set to '48'
+ * while - loop runs while 'i' is less than or equal to '57'
+ * putchar('\n') - print a newline
+ * Return: 0*
+*/
+int main(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		putchar(i);
+		i++;
+
+	}
+	putchar('\n');
+	return (0);
+}
+
+
