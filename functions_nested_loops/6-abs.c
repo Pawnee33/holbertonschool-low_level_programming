9fcf4118fdb12f7a09e56b6cc0a25c289e84952e
Pawnee <pawneedefize@hotmail.com>
2025-10-22 16:09:09 +0000

    calcule la valeur absolue d'un entier

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: the number to check
+ *
+ * Return: -n if is less than 0, n if more and equal than 0
+*/
+int _abs(int)
+{
+	if (n < 0)
+		return (-n);
+	return (n);
+}
