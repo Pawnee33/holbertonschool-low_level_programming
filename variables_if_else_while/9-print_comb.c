e978cb2f8bdec9ee5b42227307ca237ec5c81d19
Pawnee <pawneedefize@hotmail.com>
2025-10-22 09:16:01 +0000

    Affiche toutes les possibilites de combinaisons de nombres a un seul chiffre avec une virgule et un espace entre

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - My principal function
+ * int - My variable 'i' set to '48'
+ * while - loop runs while 'i' is less than or equal to '57'
+ * if - if is less than to '57', print a ',' and ' '
+ * putchar('\n') - print a newline
+ * Return: 0*
+*/
+int main(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		putchar(i);
+
+		if (i < 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
