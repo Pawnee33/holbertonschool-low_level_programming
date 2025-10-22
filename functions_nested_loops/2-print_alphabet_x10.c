f4fcb496efc420b0b7275f842ee1d97ec9847a78
Pawnee <pawneedefize@hotmail.com>
2025-10-22 13:49:13 +0000

    fonction qui affiche l'alphabet en minuscule dix foix

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints the alphabet in lowercase, followed by a new line
+ * char - My variable 'ch' set to 'a'
+ * while - loop runs while 'ch' is less than or equal to 'z'
+ * putchar(ch) - print l'alphabet to 'a' to 'z'
+ * putchar('\n') - print a newline
+ * Return: 0*
+*/
+void print_alphabet(void)
+{
+	char ch;
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		ch = 'a';
+		for (j = 0; j < 26; j++)
+		{
+			_putchar(ch++);
+		}
+		_putchar('\n');
+	}
+}
