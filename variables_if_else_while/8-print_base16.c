9d968e24f106077ddbbf8559837a14e37e799f0f
Pawnee <pawneedefize@hotmail.com>
2025-10-22 08:51:37 +0000

    Affiche tous les numeros de la base = Hexadecimal de '16' en minuscule

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
  * main - My pricipal function
- * int - My variable 'num' set to '0'
- * while - loop runs while 'num' is less than or equal to '15'
+ * int - My variable 'i'
+ * for - loop runs while 'i' is less than  to '58' up to '9' (ASCII 57)
+ * for - loop runs while 'i' is less than  to '103'up to 'f' (ASCII 102)
  * putchar(num) - print "0123456789abcdef"
  * putchar('\n') - print a newline
  * Return: 0
@@ -11,11 +12,11 @@ int main(void)
 {
 	int i;
 
-	for (i = 47; i < 58; i++)
+	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
 	}
-	for (i = 96; i < 103; i++)
+	for (i = 97; i < 103; i++)
 	{
 		putchar(i);
 	}
