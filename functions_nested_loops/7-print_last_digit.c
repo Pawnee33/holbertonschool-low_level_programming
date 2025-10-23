205354344d69db41936ae031e8ac7c96a7ac434f
Pawnee <pawneedefize@hotmail.com>
2025-10-23 09:18:26 +0000

    fonction qui affiche le dernier chiffre du numero

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * print_last_digit - computes the absolute value of an integer
- * last_digit - the number to check
+ * print_last_digit - Prints the last digit of a number
+ * @n: The number to extract the last digit from
  *
  * Return: last_digit
 */
