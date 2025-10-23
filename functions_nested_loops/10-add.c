4f5c780c11e13c9232475c46f85c96df5bf15574
Pawnee <pawneedefize@hotmail.com>
2025-10-23 15:34:01 +0000

    Fonction qui ajoute 2 entiers et retourne le resultat

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * times_table - Prints  the 9 times table, starting with 0
+ * , starting from 00:00 to 23:59
+ * int_hour - Prints 'hour'
+ * int_minutes - Prints 'minutes'
+ *
+ * Return: 0
+*/
+int add(89, 9)
