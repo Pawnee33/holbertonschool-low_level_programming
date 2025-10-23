9a248e1430152b55a82fb6ad680661dfc440c984
Pawnee <pawneedefize@hotmail.com>
2025-10-23 10:37:31 +0000

    Affiche les minutes de la journee, commenant par 00:00 et terminant par 23:59

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * jack_bauer - Prints every minute of the day
+ * , starting from 00:00 to 23:59
+ * int_hour - Prints 'hour'
+ * int_minutes - Prints 'minutes'
+ *
+ * Return: 0
+*/
+void jack_bauer(void)
+{
+    int hour;
+    int minutes;
+    for (hour = 0; hour < 24; hour++)
+    {
+        for (minutes = 0; minutes < 60; minutes++)
+        {
+            _putchar('0' + (hour / 10));
+            _putchar('0' + (hour % 10));
+            _putchar(':');
+            _putchar('0' + (minutes / 10));
+            _putchar('0' + (minutes % 10));
+            _putchar('\n');
+        }
+    }
+}
