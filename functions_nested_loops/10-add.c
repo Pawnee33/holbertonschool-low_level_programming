463a5f4d437ac228060b32e17f7e883016833b7c
Pawnee <pawneedefize@hotmail.com>
2025-10-23 16:03:07 +0000

    Fix missing semicolon in add function

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,9 +1,6 @@
 #include "main.h"
 /**
  * add - ads two integers and returns the result
- * , starting from 00:00 to 23:59
- * int_hour - Prints 'hour'
- * int_minutes - Prints 'minutes'
  *
  * Return: 0
 */
