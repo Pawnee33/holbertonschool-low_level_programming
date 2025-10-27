933492bc4d85c9aa73f789b147357bab61acf773
Pawnee <pawneedefize@hotmail.com>
2025-10-27 10:22:13 +0000

    Fonction qui multiplie deux entiers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * int_mul - Function that multiplies two integers
+ * int_a - First integer
+ * int_b - Second integer
+ *
+ * Return: 0
+*/
+int mul(int a, int b)
+{
+	mul = int a * int b
+
+	return (mul);
+}
