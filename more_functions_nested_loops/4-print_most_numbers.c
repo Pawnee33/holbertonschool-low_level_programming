c5b091e0bcfcd2b0d0eac090f63ed6395cd7d2a7
Pawnee <pawneedefize@hotmail.com>
2025-10-27 13:17:05 +0000

    Fonction qui affiche les numeros de 0 a 9, sauf le 2 et 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_numbers - Function that prints numbers (0 to 9)
+ * for - loop runs while 'i' is less to '58' up to '9'
+ * if - Check if 'i' isn't equal to '50' and '52'
+ * _putchar(i) - Prints 0 to 9
+ * _putchar('\n') - Print a newline
+ * */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		if (i != 50 && i != 52)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
+	
