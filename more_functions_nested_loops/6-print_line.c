6181211ee000dbe95561b2e734f85bdb6d4c9566
Pawnee <pawneedefize@hotmail.com>
2025-10-27 15:27:09 +0000

    Fonction qui dessine un trait du '8'

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print-line - Function that draws a straight line
+ * if - check if 'n' is more than '0'
+ * for - loop runs while 'i' is less to 'n'
+ * _putchar('_') - Prints '_'
+ * _putchar('\n') - Print a newline
+*/
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('-');
+		}
+	}
+	_putchar('\n');
+}
