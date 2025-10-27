3f1c81782615056119d60354e6a28fbe47a0e06f
Pawnee <pawneedefize@hotmail.com>
2025-10-27 14:46:40 +0000

    Fonction qui affiche 10 fois les numeros de 0 a 14

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -10,17 +10,16 @@
 */
 void more_numbers(void)
 {
-    int i, j;
-
+	int i, j;
+	
 	for (i = 0; i < 10; i++)
-    {
-    
-        for (j = 0; j <= 14; j++)
-        {
-            if (j >= 10)
-                _putchar('1');
+	{    
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+				_putchar('1');
 			_putchar('0' + (j % 10));
-        }
-        _putchar('\n');
-    }
+		}
+		_putchar('\n');
+	}
 }
