c665773d109858314f2bbcae5bf76abde6573504
Pawnee <pawneedeifze@hotmail.com>
2025-10-28 15:01:42 +0000

    Fonction qui echange deux entiers

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,11 +1,10 @@
 #include "main.h"
 /**
- * reset_to_98 - updates the value of an integer to '98'
+ * swap-int - swap int *a and *b
  * @a: pointer to an interger to update
  * @b: pointer to an interger to update
  * int paw - set to '*a'
- * Description: This function takes a pointer to an int as a parameter
- * and changes the value of the integer it points to to 98.
+ * Description: function that swaps the values of two integers
 */
 void swap_int(int *a, int *b)
 {
