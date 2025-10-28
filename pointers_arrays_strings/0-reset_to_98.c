2ae5a42ae1a671ccc80da000de9307f5659dc88f
Pawnee <pawneedeifze@hotmail.com>
2025-10-28 14:26:38 +0000

    Fonction qui prend un pointeur d'un entier comme parametre et change la valeur en 98

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * reset_to_98 - updates the value of an integer to '98'
- * @*n: pointer to an interger to update
+ * @n: pointer to an interger to update
  * Description: This function takes a pointer to an int as a parameter
  * and changes the value of the integer it points to to 98.
 */
