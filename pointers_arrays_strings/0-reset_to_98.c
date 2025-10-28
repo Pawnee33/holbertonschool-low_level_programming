f727bd051c5beaa24a962d945a39f95992dc6d9a
Pawnee <pawneedeifze@hotmail.com>
2025-10-28 14:20:19 +0000

    Fonction qui prend un pointeur d'un entier comme parametre et change la valeur en 98

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,7 @@
 #include "main.h"
 /**
- * print_triangle - Function that prints a square
- * @size: number of times the character '#' should be printed
- * if - check if 'size' is less and equal to '0'
- * for - loop runs while 'i' is less to 'size'
- * for - loop runs while 'j' is less to 'size - i'
- * for - loop runs while 'j' is less to 'i'
- * _putchar(' ') - Prints '#'
- * _putchar('\n') - Print a newline
+ * reset_to_98 - Function that updates the value to '98'
+ * @*n: 98
 */
 void reset_to_98(int *n)
 {
