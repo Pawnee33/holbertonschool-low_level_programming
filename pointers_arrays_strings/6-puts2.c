6d785433515bf6ae0b62e4253bfe6c6e0c96d08c
Pawnee <pawneedefize@hotmail.com>
2025-10-29 14:39:05 +0000

    Fonction qui affiche un caracteres sur 2, en commenant parle premier

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _puts2 - Function that prints every other character of a string
+ * puts2 - Function that prints every other character of a string
  * , starting with the first character, followed by a new line
  * @str: the string we want to know
  * int i - set to '0'
