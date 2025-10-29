29b01a4fd84eadf089e1e834ea319fe9d861a887
Pawnee <pawneedefize@hotmail.com>
2025-10-29 14:56:07 +0000

    Fonction qui affiche un caracteres sur 2, en commenant parle premier

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -8,9 +8,13 @@
 */
 void puts2(char *str)
 {
+int len;
 int i;
 
-for (i = 0; str[i] != '\0'; i += 2)
+while (str[len] != '\0')
+	len++;
+
+for (i = 0; i < len; i += 2)
 	_putchar(str[i]);
 
 _putchar('\n');
