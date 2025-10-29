2e9c298d8ac9d61dce5c9283841814c12184711d
Pawnee <pawneedefize@hotmail.com>
2025-10-29 08:58:45 +0000

    Fonction qui retourne la longueur d'une chaine de caracters

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen -  function that returns the length of a string
+ * int len - set to '0'
+ * for - run through the array 's'
+ * Return: len
+*/
+int _strlen(char *s)
+{
+int len = 0;
+
+for (len = 0; s[len] != '\0'; len++)
+{
+}
+return (len);
+}
