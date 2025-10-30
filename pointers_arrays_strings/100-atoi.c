a1b91fa54b7db784c51c13c65917ba6137d7cf3a
Pawnee <pawneedefize@hotmail.com>
2025-10-30 13:43:16 +0000

    Fonction qui convertit une chaine de caractere en entier

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -11,26 +11,29 @@
 */
 int _atoi(char *s)
 {
-    int i = 0;
-    int sign = 1;
-    int result = 0;
+    int i = 0, sign = 1, result = 0;
+    int found_digit = 0;
 
-    while (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' ||
-           s[i] == '\v' || s[i] == '\f' || s[i] == '\r')
-        i++;
-
-    if (s[i] == '-' || s[i] == '+')
+    while (s[i] != '\0' && !found_digit)
     {
         if (s[i] == '-')
-            sign = -1;
+            sign *= -1;
+        else if (s[i] >= '0' && s[i] <= '9')
+            found_digit = 1;
         i++;
     }
 
-    while (s[i] >= '0' && s[i] <= '9')
+    if (found_digit)
     {
-        result = result * 10 + (s[i] - '0');
-        i++;
+        i--;
+        while (s[i] >= '0' && s[i] <= '9')
+        {
+            result = result * 10 + (s[i] - '0');
+            i++;
+        }
     }
+	else
+	i++;
 
     return result * sign;
 }
