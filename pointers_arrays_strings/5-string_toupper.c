18a41a0e950c2bdbae14977ae08ab248d2024a20
Pawnee <pawneedefize@hotmail.com>
2025-10-31 13:17:44 +0000

    Fonction qui change les lettre minuscule d'une chaine de caracteres en majuscule

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,23 +1,25 @@
 #include "main.h"
 /**
- * reverse_array - reverse my array 'a'
- * @a: pointer to my string 'a'
- * @n: number of elements to print
- * int start - set to '0'
- * int end - set to "n - 1"
- * int temp - set to array 'a'
- * Description: function that reverse an array
+ * *string_toupper - Converts all lowercase letters of a string to uppercase
+ * @str: The string to modify
+ *
+ * Description: This function scans through the string `str` and converts
+ * every lowercase letter ('a' to 'z') into its corresponding uppercase
+ * letter ('A' to 'Z'). It does so by subtracting 32 from the ASCII value
+ * of each lowercase letter, which works because of the way ASCII is ordered.
+ *
+ * Return: A pointer to the modified string `str`.
 */
 char *string_toupper(char *str)
 {
-    int i;
+	int i;
 
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        if (str[i] >= 'a' && str[i] <= 'z')
-        {
-            str[i] = str[i] - 32;
-        }
-    }
-    return (str);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
 }
