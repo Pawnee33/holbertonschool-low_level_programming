14e63e6416a916e058530deda787adaef98d2270
Pawnee <pawneedefize@hotmail.com>
2025-11-03 17:00:32 +0000

    Fonction qui affiche l'echiquier

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_chessboard - prints an 8x8 chessboard
+ * @a: pointer to an array of 8 charcters
+ * @needle: substring to find
+ * for - loop through each row of the chessboard
+ * for - loop through each column of the current row
+ * Description: This function prints the chessboard
+ */
+void print_chessboard(char (*a)[8])
+{
+	int lignes;
+	int colonnes;
+
+	for (lignes = 0; lignes < 8; lignes++)
+	{
+		for (colonnes = 0; colonnes < 8; colonnes++)
+		{
+			_putchar(a[lignes][colonnes]);
+		}
+		_putchar('\n');
+	}
+}
