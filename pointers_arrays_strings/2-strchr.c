d4cc57bcc45552afb919086ba93dc7319d235902
Pawnee <pawneedefize@hotmail.com>
2025-11-03 13:14:35 +0000

    Fonction qui localise un caractere dans une chaine de caracteres

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -3,10 +3,6 @@
  * *_strchr - Function that locates a character in a string
  * @s: pointer to the string to search in
  * @c: Charcter to locate
- * for - loop through the string until ce reach the null terminator
- * if - check if the current character matches 'c'
- * , return pointer to the matching character
- * if - check if the current character matches '\0'
  * , return pointer to the matching character
  * Return: NULL
 */
@@ -14,14 +10,14 @@ char *_strchr(char *s, char c)
 {
 	unsigned int i = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (s[i] != '\0' && s[i] != c)
 	{
-		if (s[i] == c)
+		i++;
+
+		if (s[i] == '\0' || s[i] == c)
 		{
-			return (s + i);
+			return (&s[i]);
 		}
-		if (c == '\0')
-			return (s + i);
 	}
 	return (NULL);
 }
