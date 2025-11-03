36f655592f70d144fa6f0458127bddb25324937c
Pawnee <pawneedefize@hotmail.com>
2025-11-03 12:03:13 +0000

    Fonction qui localise un caractere dans une chaine de caracteres

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -14,14 +14,14 @@ char *_strchr(char *s, char c)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == c)
+		if (s[i] == c && s[i] == '\0')
 		{
 			return (s + i);
 		}
-		if (s[i] == '\0')
+		else
 		{
-			return (s + 1);
+			return (NULL);
 		}
 	}
-	return (NULL);
 }
+
