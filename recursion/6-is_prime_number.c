50c6c514cda197e8b0d8aef053ba7a1c6304708a
Pawnee <pawneedefize@hotmail.com>
2025-11-04 17:43:06 +0000

    Fonction qui renvoie '1' si l'entier entre est un nombre premier, sinon elle renvoie 0

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _prime_number_test - recursively checks if a number has a divisor
+ * @n: number to check
+ * @test: current divisor to test
+ *
+ * Return: 1 if n is prime, 0 otherwise
+ */
+int _prime_number_test(int n, int test)
+{
+    if (test == n)
+        return (1); 
+    if (n % test == 0)
+        return (0); 
+    return (_prime_number_test(n, test + 1));
+}
+
+/**
+ * is_prime_number - returns 1 if n is prime, 0 otherwise
+ * @n: number to check
+ *
+ * Return: 1 if n is prime, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+    if (n <= 1)
+        return (0);
+    return (_prime_number_test(n, 2));
+}
