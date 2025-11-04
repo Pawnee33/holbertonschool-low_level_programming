45e5e38eaab2bccc01cc51c58f758ecd55475f4d
Pawnee <pawneedefize@hotmail.com>
2025-11-04 12:28:20 +0000

    Fonction qui affive une chaine de caracteres a l'envers

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - print a string reverse
+ * @s: the string to be printed
+ * if - Base case: if we reach the end of the string
+ * _putchar - print a new line
+ * Return: exit the function
+ *  _print_rev_recursion(s + 1) - recusive call for the rest of the string
+ * _putchar(*s) - print the current character
+
+*/
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
