51f0449fb501cbf2e8ecf9b7016b86c983724111
Pawnee <pawnedefize@hotmail.com>
2025-11-05 11:30:15 +0000

    Affiche le numero de l'argument

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - Function that prints the number of arguments
+* passed into it
+* @argc: number of command-line arguments
+* @argv: array of command-line
+* Return: 0
+*/
+int main(int argc, char **argv)
+{
+	(void)argv;
+	printf("argc = %d\n", argc);
+	return (0);
+}
