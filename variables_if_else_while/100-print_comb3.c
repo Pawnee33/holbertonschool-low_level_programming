12508b6ffc9e1d2e4ad05f0a13dd6dad7f938fac
Pawnee <pawneedefize@hotmail.com>
2025-11-06 10:39:54 +0000

    Programme qui affiche toutes les possibilitees de combinaisons differentes de deux nombre

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -1,30 +1,30 @@
 #include <stdio.h>
 /**
-* main - Function that prints all possible 
+* main - Function that prints all possible
 * different combinations of two digits
-* @i: My first digit
-* @j: My second digit
-* 
+* int i - My first digit
+* int j - My second digit
+*
 * Return: 0
 */
 int main(void)
 {
-    int i = 0;
-    int j = 0;
+	int i = 0;
+	int j = 0;
 
-    for (i = 0; i < 9; i++)
-    {
-        for (j = i + 1; j < 10; j++)
-        {
-        if (!(i == 0 && j == 1))
-        {
-            putchar(',');
-            putchar(' ');
-        }
-        putchar('0' + i);
-        putchar('0' + j);
-        }
-    }
-    putchar('\n');
-    return (0);
+	for (i = 0; i < 9; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+		if (!(i == 0 && j == 1))
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		putchar('0' + i);
+		putchar('0' + j);
+		}
+	}
+	putchar('\n');
+	return (0);
 }
