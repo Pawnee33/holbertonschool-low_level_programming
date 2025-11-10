0a8774efdd1052e26cc017653fcdd0fcb2004144
Pawnee <pawneedefize@hotmail.com>
2025-11-10 16:09:07 +0000

    Fonction qui concatene deux chaine de caracteres

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -13,15 +13,18 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
-		return (NULL);
+		s1 = "";
 	}
 
 	if (s2 == NULL)
 	{
-		return (NULL);
+		s2 = "";
 	}
-
-	concate = malloc(strlen(s1) + strlen(s2) + 1);
+	for (i = 0; s1[i] != '\0'; i++)
+	;
+	for (j = 0; s2[j] != '\0'; j++)
+	;
+	concate = malloc(sizeof(char) * (i + j + 1));
 
 	if (concate == NULL)
 	return (NULL);
