506e890c1781a42e30d39eaa0f6a4421bf276a55
Pawnee <pawneedefize@hotmail.com>
2025-11-13 14:24:18 +0000

    Fonction qui affiche une structure de chien

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -8,7 +8,7 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
-		printf("Age: (nil)%.6f\n", d->age);
-		printf("Owner: (nil)%s\n", (d->owner != NULL) ? d->owner : "(nil)");
+		printf("Age: %.6f\n", d->age);
+		printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
 	}
 }
