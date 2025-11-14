703051ea6e488e55eb28f40a4bfa0d5128a031c9
Pawnee <pawneedefize@hotmail.com>
2025-11-14 09:52:49 +0000

    Fonction qui liberes 'dogs'

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+/**
+* free_dog - That free dogs
+* @d: dogs to free
+*/
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+
+}
