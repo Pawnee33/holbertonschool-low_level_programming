1d5dd164b22b0708f9b58482c05ff9341ba8fca0
Pawnee <pawnedefize@hotmail.com>
2025-11-17 15:48:14 +0000

    Fonvtion qui chercher pour un entier

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -2,24 +2,25 @@
 #include <stdlib.h>
 
 /**
-* array_iterator - Function that executes a function
-* given as a parameter on each element of an array
-* @array: The array to iterates
-* @size: size of my array
-* @action: my pointer to the function to ececutes
-* Return: NULL
-*/
+ * int_index - searches for an integer in an array
+ * @array: pointer to the array to be searched
+ * @size: number of elements in the array
+ * @cmp: pointer to the function used to compare values
+ *
+ * Return: the index of the first element for which cmp does not return 0,
+ * otherwise -1. Also returns -1 if size <= 0 or if array or cmp is NULL.
+ */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
 	if (cmp == NULL || size <= 0)
-		return(-1);
+		return (-1);
 	{
 		while (i < size)
 		{
 			cmp(array[i]);
-			if (cmp(array[i]) != 0) 
+			if (cmp(array[i]) != 0)
 				return (i);
 			i++;
 		}
