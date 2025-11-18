42902aa155fa33ff929757237dd39e607752fdc7
Pawnee <pawneedefize@hotmail.com>
2025-11-18 13:04:55 +0000

    Fonction qui fait une calculatrice

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,21 +1,29 @@
 #include "3-calc.h"
 #include <string.h>
+/**
+* *get_op_func - Function that selects the correct
+* , function for the given operator
+* @s: the oerator passed as argument ("+", "-", "*", "/", "%")
+*
+* Return: NULL or pointer to the right function
+*/
 int (*get_op_func(char *s))(int, int)
 {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
-    while (ops[i].op != NULL)
-    {
-        if (strcmp(s, ops[i].op) == 0)
-        return (ops[i].f);
-        i++;
-    }
-    return (NULL);
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (ops[i].op != NULL)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+		return (ops[i].f);
+		i++;
+	}
+	return (NULL);
 }
