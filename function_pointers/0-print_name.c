8948d5f02b1f084ddc6cb24c37c858466e3fb8d6
Pawnee <pawneedefize@hotmail.com>
2025-11-18 09:46:40 +0000

    Fonction qui affiche un nom

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -4,10 +4,12 @@
 * print_name - print a name
 * @name: The name to be prints
 * @f: My function pointer
+* Description - Function that print a name
 */
 void print_name(char *name, void (*f)(char *))
 {
-	if (f == NULL)
-	return;
+	if (f == NULL || name == NULL)
+		return;
+
 	f(name);
 }
