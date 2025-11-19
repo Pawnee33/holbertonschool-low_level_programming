569043ef51b94cd9c85886f4c29ef7ecf2eb92e6
Pawnee <pawneedefize@hotmail.com>
2025-11-19 16:32:40 +0000

    Fonction qui retourne le resultat de plusieurs parametres

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "variadic_functions.h"
 /**
 * print_numbers - Prints numbers with a separator ','
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -3,7 +3,6 @@
 
 #include <stdlib.h>
 #include <stdarg.h>
-#include <stdio.h>
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
