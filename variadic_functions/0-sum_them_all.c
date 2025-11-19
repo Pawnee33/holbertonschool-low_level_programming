ab141353c57b705f69c0dd07ae21eaca339af2f8
Pawnee <pawneedefize@hotmail.com>
2025-11-19 16:28:51 +0000

    Fonction qui retourne le resultat de plusieurs parametres

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -11,10 +11,10 @@ int sum_them_all(const unsigned int n, ...)
 	int result = 0;
 	unsigned int i;
 
-	va_start(ap, n);
-
 	if (n == 0)
 		return (0);
+
+	va_start(ap, n);
 
 	for (i = 0; i < n; i++)
 	{
