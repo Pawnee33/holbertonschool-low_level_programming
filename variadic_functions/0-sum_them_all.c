24cc2955e8cd9015f53d77daed726fe5c58d0563
Pawnee <pawneedefize@hotmail.com>
2025-11-20 10:27:18 +0000

    rectification du code

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,14 +1,14 @@
 #include "variadic_functions.h"
 /**
-* sum_them_all - Function that result of all its parameters
-* @n: the size of numbers to result
+* sum_them_all - Function that sum of all its parameters
+* @n: the size of numbers to sum
 *
-* Return: The result of my parameters and 0 if n equal 0
+* Return: The sum of my parameters and 0 if n equal 0
 */
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
-	int result = 0;
+	int sum = 0;
 	unsigned int i;
 
 	if (n == 0)
@@ -18,8 +18,8 @@ int sum_them_all(const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		result += va_arg(ap, int);
+		sum += va_arg(ap, int);
 	}
 	va_end(ap);
-	return (result);
+	return (sum);
 }
