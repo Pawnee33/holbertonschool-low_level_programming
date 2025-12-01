92825874d609e9f41cdeadc84345fd61bb21f392
Pawnee <pawneedefize@hotmail.com>
2025-12-01 13:05:43 +0000

    Function that print list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,10 +1,16 @@
 #include "lists.h"
 /**
+* print_list - Prints all elements of list_t
+* @h: My pointer that point to ma list_t
+*
+* Return: the numbers of elements in a linked
+* list_t
 */
 size_t print_list(const list_t *h)
 {
 	const list_t *tmp = h;
 	size_t add = 0;
+
 	while (tmp != NULL)
 	{
 		if (tmp->str == NULL)
