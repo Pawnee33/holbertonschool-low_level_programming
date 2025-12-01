fc606d10ecb4af866e1b3089fbb6b3b0f831437e
Pawnee <pawneedefize@hotmail.com>
2025-12-01 14:57:52 +0000

    add new node of the beginning of list_t

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,14 +1,28 @@
 #include "lists.h"
 /**
-* list_len - returns the number of elements of list_t
-* @h: My pointer that point to ma list_t
+* add_node - add a new node to my list_t
+* @head: The adress of the first node to ma list_t
+* @str: My string to duplicated and store in my new node
 *
-* Return: the numbers of elements in a linked
-* list_t
+* Return: The adress of my new node or NULL
 */
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new_node;
-    new_node = malloc(sizeof(list_t));
-    new_node-> = str;
+	list_t *new_node;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->len = strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
 }
