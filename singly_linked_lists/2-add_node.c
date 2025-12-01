d59131d9801b95e222718c90cff632379abc5015
Pawnee <pawneedefize@hotmail.com>
2025-12-01 14:06:49 +0000

    function that returns the numbers of the elements in a linked list-t

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+/**
+* list_len - returns the number of elements of list_t
+* @h: My pointer that point to ma list_t
+*
+* Return: the numbers of elements in a linked
+* list_t
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+    list_t *new_node;
+    new_node = malloc(sizeof(list_t));
+    new_node-> = str;
+}
