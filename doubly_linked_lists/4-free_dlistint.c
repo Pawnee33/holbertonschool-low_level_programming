bdaa5e8f1ab31d14989e567c877e8f42e4b5191a
Pawnee <pawneedefize@hotmail.com>
2025-12-03 08:31:25 +0000

    Frees a dlistint_t list

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <string.h>
+/**
+* free_dlistint - frees a dlistint_t list
+* @head: Pointer to the pointer to the head of the list
+* dlistint_t keep_address - Keep address of next node before to free
+*/
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *keep_address = head;
+
+	while (keep_address != NULL)
+	{
+		dlistint_t *next = keep_address->next;
+		free(keep_address);
+		keep_address = next;
+	}
+}
