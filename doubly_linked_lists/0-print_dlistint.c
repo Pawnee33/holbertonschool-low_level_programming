7d61ba9a897ce83c315abd070dd48ef06affec0a
Pawnee <pawneedefize@hotmail.com>
2025-12-03 08:20:38 +0000

    clean the description of print_dlistint_t in dlistint

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 /**
-* print_dlistint_t - Prints all elements of dlistint_t
+* print_dlistint - Prints all elements of dlistint_t list
 * @h: My pointer that point to a dlistint_t
 *
 * Return: the numbers of elements in a linked
-* dlistint_t
+* dlistint_t list
 */
 size_t print_dlistint(const dlistint_t *h)
 {
