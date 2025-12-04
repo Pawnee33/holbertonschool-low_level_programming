33de3db3970cdde67a12b584eaae5a8ec194e9f4
Pawnee <pawneedefize@hotmail.com>
2025-12-04 15:45:14 +0000

    deletes the node at the index of a dlistint_t list

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -13,37 +13,34 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	unsigned int i = 0;
 
 	if (*head == NULL)
-	{
 		return (-1);
-	}
+
 	if (index == 0)
 	{
 		tmp = *head;
 		*head = (*head)->next;
 		if (*head != NULL)
-		{
 			(*head)->prev = NULL;
-		}
+
 		free(tmp);
 		return (1);
 	}
+
 	tmp = *head;
+
 	for (i = 1; tmp != NULL && i < index; i++)
 	{
 		tmp = tmp->next;
 		if (tmp == NULL)
-		{
 			return (-1);
-		}
 	}
+
 	if (tmp->prev != NULL)
-	{
 		tmp->prev->next = tmp->next;
-	}
+
 	if (tmp->next != NULL)
-	{
 		tmp->next->prev = tmp->prev;
-	}
+
 	free(tmp);
 	return (1);
 }
